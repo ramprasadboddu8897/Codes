8f59344555a70d7e206dddff4f345060a4037944
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2023-12-19 11:51:07 +0530

    Update FrequencyCounter.java

--- trainee3/FrequencyCounter.java
+++ trainee3/FrequencyCounter.java
@@ -2,62 +2,63 @@ package com.phenom.trainee3;
 
 public class FrequencyCounter {
 
-	    public static void main(String[] args) {
-	        int[] array = {1, 2, 3, 2, 4, 1, 5, 3, 4, 5, 2, 1};
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 2, 4, 1, 5, 3, 4, 5, 2, 1, 100001, 1001,1001};
 
-	        // Assuming the array contains positive integers
-	        int maxElement = findMaxElement(array);
+        // Assuming the array contains positive integers
+        int maxElement = findMaxElement(array);
 
-	        // Initialize a frequency array with zeros
-	        int[] frequencyArray = new int[maxElement + 1];
-	        
-	        //System.out.println(Arrays.toString(frequencyArray));
+        // Initialize a frequency array with zeros
+        int[] frequencyArray = new int[maxElement + 1];
 
-	        // Update the frequencies
-	        updateFrequencies(array, frequencyArray);
+        // Update the frequencies
+        updateFrequencies(array, frequencyArray);
 
-	        // Display the frequencies
-	        displayFrequencies(frequencyArray);
-	        
-	        // Finding Most Frequent Element
-	        int maxFreqEle=maxFrequencyEle(array);
-	        System.out.println(maxFreqEle);
-	    }
+        // Display the frequencies
+        displayFrequencies(frequencyArray);
 
-	    static int findMaxElement(int[] array) {
-	        int maxIndex = 0; // Assume the first element is the maximum
+        // Finding Most Frequent Element
+        int maxFreqEle = maxFrequencyEle(array);
+        System.out.println(maxFreqEle);
+    }
+
+    static int findMaxElement(int[] array) {
+        int maxIndex = 0; // Assume the first element is the maximum
 
         for (int i = 1; i < array.length; i++) {
             if (array[i] > array[maxIndex]) {
                 maxIndex = i; // Update the maxIndex if a larger element is found
-            	}
-        	}
-        return maxIndex;
-	    }
-	    
-	    static int maxFrequencyEle(int[] array) {
-	    	int maxFreqEle=Integer.MIN_VALUE;
-	    	for (int num:array) {
-	    		if(num>maxFreqEle) {
-	    			maxFreqEle=num;
-	    		}
-	    	}
-	    	return array[maxFreqEle];
-	    	
-	    }
+            }
+        }
+        return array[maxIndex]; // Return the maximum element itself, not its index
+    }
 
-	    static void updateFrequencies(int[] array, int[] frequencyArray) {
-	        for (int value : array) {
-	            frequencyArray[value]++;
-	        }
-	    }
+    static int maxFrequencyEle(int[] array) {
+        int maxFreqEle = Integer.MIN_VALUE;
+        for (int num : array) {
+            if (num > maxFreqEle) {
+                maxFreqEle = num;
+            }
+        }
+        return maxFreqEle;
+    }
 
-	    static void displayFrequencies(int[] frequencyArray) {
-	        System.out.println("Frequency of each number:");
-	        for (int i = 0; i < frequencyArray.length; i++) {
-	            if (frequencyArray[i] > 0) {
-	                System.out.println(i + ": " + frequencyArray[i] + " times");
-	            }
-	        }
-	    }
-	}
+    static void updateFrequencies(int[] array, int[] frequencyArray) {
+        for (int value : array) {
+            if (value < frequencyArray.length) {
+                frequencyArray[value]++;
+            } else {
+                System.out.println("Ignoring out-of-bound value: " + value);
+            }
+        }
+    }
+
+    static void displayFrequencies(int[] frequencyArray) {
+        System.out.println("Frequency of each number:");
+        for (int i = 0; i < frequencyArray.length; i++) {
+            if (frequencyArray[i] > 0) {
+                System.out.println(i + ": " + frequencyArray[i] + " times");
+            }
+        }
+    }
+}
